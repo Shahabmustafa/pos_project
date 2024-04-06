6a01d69195181b0698c0159ee803a42a66587227
Shahabmustafa <shahabmustafa57@gmail.com>
2024-04-06 07:58:38 +0500

    this screen i add supplier ui

--- lib/view/supplier/supplier_screen.dart
+++ lib/view/supplier/supplier_screen.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import '../../res/component/app_textfield.dart';
 
 class SupplierScreen extends StatefulWidget {
   const SupplierScreen({super.key});
@@ -10,6 +13,76 @@ class SupplierScreen extends StatefulWidget {
 class _SupplierScreenState extends State<SupplierScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: (){
+          showDialog(
+            context: context,
+            builder: (context){
+              return AlertDialog(
+                content: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    AppTextField(
+                      title: "Supplier Name",
+                      hintText: "Enter Supplier Name",
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    AppTextField(
+                      title: "Supplier Email",
+                      hintText: "Enter Email",
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    AppTextField(
+                      title: "Supplier Phone Number",
+                      hintText: "Enter Phone Number",
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Row(
+                     children: [
+                       Flexible(
+                         child: AppTextField(
+                           title: "Receiver Balance",
+                           hintText: "Enter Receiver Balance",
+                         ),
+                       ),
+                       SizedBox(
+                         width: 10,
+                       ),
+                       Flexible(
+                         child: AppTextField(
+                           title: "Sender Balance",
+                           hintText: "Enter Sender Balance",
+                         ),
+                       ),
+                     ],
+                   ),
+                  ],
+                ),
+                actions: [
+                  ElevatedButton(
+                    onPressed: (){},
+                    child: Text("Add Supplier"),
+                  ),
+                  ElevatedButton(
+                    onPressed: (){
+                      Navigator.pop(context);
+                    },
+                    child: Text("Cancel"),
+                  ),
+                ],
+              );
+            },
+          );
+        },
+        label: Text("Add Supplier"),
+      ),
+    );
   }
 }
